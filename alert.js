75a949bd3dc5a4f2a529ceca33df02f8006afaf3
Dane Williamson <danewilliamson@danes-mbp.raleigh.ibm.com>
2018-05-30 16:00:20 -0400

    esLint and Js-Beautify configured

---
+++ alert.js
@@ -0,0 +1,33 @@
+"use strict";
+
+let student = {
+  name: 'John',
+  age: 30,
+  isAdmin: false,
+  courses: ['html', 'css', 'js'],
+  wife: null
+};
+
+let json = JSON.stringify(student);
+
+function logSomething() {
+  console.log("Something");
+}
+
+console.log(json);
+
+// alert(typeof json); // we've got a string!
+//
+//
+//
+// alert(json);
+/* JSON-encoded object:
+{
+  "name": "John",
+  "age": 30,
+  "isAdmin": false,
+  "courses": ["html", "css", "js"],
+  "wife": null
+}*/
+
+// console.log(json);
